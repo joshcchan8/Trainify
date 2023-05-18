99946eb84fba1544af51b313a152e192b616f0bb
joshc4 <joshchan4444@gmail.com>
2023-05-17 20:35:55 -0700

    established DB connection

--- server/server.go
+++ server/server.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+
 	"github.com/gin-gonic/gin"
-	// "database/sql"
-	// "github.com/go-sql-driver/myysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	// Connect to DB
+	db, err := sql.Open("mysql", "trainee:8787@tcp(localhost:3306)/database")
+	if err != nil {
+		return
+	}
+	defer db.Close()
+	fmt.Println("Connected to DB")
+
+	// Router
 	router := gin.Default()
 	router.Run("localhost:3000")
 }
