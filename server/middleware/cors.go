bc904c34588e530106fcc5e8bbb3a376dc24e3b9
joshc4 <joshchan4444@gmail.com>
2024-01-14 22:23:01 -0800

    updated cors middleware

--- server/middleware/cors.go
+++ server/middleware/cors.go
@@ -8,9 +8,18 @@ import (
 
 func CorsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// Allow requests only from 'http://localhost:3000'
 		c.Header("Access-Control-Allow-Origin", "*")
+
+		// Allow necessary HTTP methods
 		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
+
+		// Allow necessary headers
 		c.Header("Access-Control-Allow-Headers", "Origin, Authorization, Content-Type")
+
+		// Allow credentials
+		c.Header("Access-Control-Allow-Credentials", "true")
+
 		c.Status(http.StatusOK)
 		c.Next()
 	}
